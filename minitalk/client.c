a7e8714d0bf36e513bfed2e06a431391db197526
El mehdi El orchi <eel-orch@e1r5p12.1337.ma>
2021-06-06 20:24:59 +0100

    kdjnfgfnjgfbdgjfgkdsgdfglgksfdsldfdlfdskld

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,20 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/06 14:32:10 by eel-orch          #+#    #+#             */
+/*   Updated: 2021/06/06 16:30:16 by eel-orch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int *pinary_to_decimal(char c)
+{
+	int		*bin;
+	int		decimal;
+	int		i;
+	int		j = 0;
+
+	i = 7;
+	bin = (int *)malloc(sizeof(int) * 8);
+	while (j < 8)
+	{
+		bin[j] = 0;
+		j++;
+	}
+	decimal = c - '0';
+	while (decimal != 0)
+	{
+		bin[i] = decimal % 2;
+		decimal /= 2;
+		i--;
+	}
+	i = 0;
+	while (i < 8)
+	{
+		printf("%d", bin[i]);
+		i++;
+	}
+	return (bin);
+}
+
+char decimal_to_binary(int *bin)
+{
+	char c;
+	return (c);
+}
 
 int main(int argc, char const *argv[])
 {
-    int i;
-    int j;
-        
-    i = 0;
-    while (argv[1][i])
-    {
-        j = 0;
-        while (j < 8)
-        {
-            //Encode each character to binary
-            //IF zero send SIGUSR2; ELSE send SIGUSR2
-            //
-        }
-    }
-    return 0;
+	int j;
+	int	i;
+	int *bin;
+	char c;
+
+	i = 0;
+	while (argv[2][j] != '\0')
+	{
+		bin = pinary_to_decimal((char )argv[2][j]);
+		while (i < 8)
+		{
+			if (bin[i] == 0)
+				kill(argv[1], SIGUSR1);
+			else
+				kill(argv[1], SIGUSR2);
+		}
+		j++;
+		free(bin);
+	}
+	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/06 15:16:20 by eel-orch          #+#    #+#             */
+/*   Updated: 2021/06/06 15:16:22 by eel-orch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 
 #include <unistd.h>
@@ -13,16 +25,13 @@ void function(int sig)
 
 int main(int argc, char const *argv[])
 {
-    pid_t pid = getpid();
+    pid_t pid;
 
+	pid = getpid();
     printf("PDI: %d\n", pid);
-
-
-    signal(9, function);
-    // signal(SIGUSR2, function);
-
-    while (0x1)
+    signal(SIGUSR1, function);
+    signal(SIGUSR2, function);
+    while (1)
         pause();
-
     return 0;
 }
